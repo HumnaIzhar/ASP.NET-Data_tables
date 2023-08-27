ce699c0b875881802d9b20c62fd73cf62aed4e69
HumnaIzhar <107610302+HumnaIzhar@users.noreply.github.com>
2023-08-27 14:48:01 -0400

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,18 @@
+﻿create table employee(
+id int primary key,
+name varchar (50) not null,
+gender varchar (50) not null,
+designation varchar (50) not null,
+city varchar (50) not null,
+salary int not null
+);
+
+insert into employee values 
+(1,'iqra','female','teacher','karachi',12000),
+(2,'saad','male','teacher','karachi',12000),
+(3,'sana','female','teacher','karachi',12000),
+(4,'hira','female','teacher','karachi',12000),
+(5,'faisal','male','teacher','karachi',12000),
+(6,'haris','male','teacher','karachi',12000),
+(7,'rafay','male','teacher','karachi',12000),
+(8,'yumna','female','teacher','karachi',12000);
